0bead50e78903de3a1bd7010dec0ebe1e1a1f408
WilfredoGuerra <willigue@gmail.com>
2020-04-15 16:33:18 -0500

    create-edit articulo

--- resources/views/almacen/articulo/edit.blade.php
+++ resources/views/almacen/articulo/edit.blade.php
@@ -3,7 +3,7 @@
 @section('contenido')
     <div class="row">
         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-            <h3>Editar Categoría: {{ $categoria->nombre }}</h3>
+            <h3>Editar Articulo: {{ $articulo->nombre }}</h3>
             @if (count($errors)>0)
             <div class="alert alert-danger">
                 <ul>
@@ -13,26 +13,69 @@
                 </ul>
             </div>
             @endif
+        </div>
+    </div>
 
-            {!! Form::model($categoria, ['method' => 'PATCH', 'route' => ['categoria.update', $categoria->idcategoria]])!!}
-            {!! Form::token() !!}
+        {!! Form::model($articulo, ['method' => 'PATCH', 'route' => ['articulo.update', $articulo->idarticulo], 'files'=>'true'])!!}
+        {!! Form::token() !!}
+        <div class="row">
+            <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
                 <div class="form-group">
                     <label for="nombre">Nombre</label>
-                    <input type="text" name="nombre" class="form-control" value="{{$categoria->nombre}}" placeholder="Nombre...">
+                    <input type="text" name="nombre" required value="{{$articulo->nombre}}" class="form-control">
                 </div>
-
+            </div>
+            <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+                <div class="form-group">
+                    <label for="">Categoría</label>
+                    <select name="idcategoria" class="form-control" id="">
+                        @foreach ($categorias as $cat)
+                            @if($cat->idcategoria==$articulo->idcategoria)
+                            <option value="{{$cat->idcategoria}}" selected>{{$cat->nombre}}</option>
+                            @else
+                            <option value="{{$cat->idcategoria}}">{{$cat->nombre}}</option>    
+                            @endif
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+                <div class="form-group">
+                    <label for="codigo">Código</label>
+                    <input type="text" name="codigo" required value="{{$articulo->codigo}}" class="form-control">
+                </div>
+            </div>
+            <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+                <div class="form-group">
+                    <label for="stock">Stock</label>
+                    <input type="text" name="stock" required value="{{$articulo->stock}}" class="form-control">
+                </div>
+            </div>
+            <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
                 <div class="form-group">
                     <label for="descripcion">Descripción</label>
-                    <input type="text" name="descripcion" class="form-control" value="{{$categoria->descripcion}}" placeholder="Descripción...">
+                    <input type="text" name="descripcion" value="{{$articulo->descripcion}}" class="form-control" placeholder="Descripción...">
                 </div>
-
+            </div>
+            <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+                <div class="form-group">
+                    <label for="imagen">Imagen</label>
+                    <input type="file" name="imagen" class="form-control">
+                    @if(($articulo->imagen)!="")
+                        <img src="{{asset('imagenes/articulos/' .$articulo->imagen)}}" height="300px" width="300px" alt="">
+                    @endif
+                </div>
+            </div>
+    
+            <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
                 <div class="form-group">
                     <button class="btn btn-primary" type="submit">Guardar</button>
                     <button class="btn btn-danger" type="reset">Cancelar</button>
                 </div>
+            </div>
+    
+        </div>                
 
-            {!! Form::close() !!}
+        {!! Form::close() !!}
 
-        </div>
-    </div>
 @endsection
